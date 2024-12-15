edc9969b6c95054d237129d4975483a9d67bf8a2
Hasan Mustafa ERGEN <h.mustafaergen@gmail.com>
2024-12-16 01:11:37 +0300

    Update

---
+++ PartsManagementTool.Core/Concretes/BodyParts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsManagementTool.Core.Concretes
+{
+    internal class BodyParts
+    {
+    }
+}
